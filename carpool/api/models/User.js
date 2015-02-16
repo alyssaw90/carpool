56069f23c6872a919dcfb18ecf361f0b8287bf69
Alyssa <alyssaw90@gmail.com>
2015-02-16 11:05:30 -0800

    user model

--- carpool/api/models/User.js
+++ carpool/api/models/User.js
@@ -5,10 +5,40 @@
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
+var bcrypt = require('bcrypt');
+
 module.exports = {
 
   attributes: {
-
+    firstName: {
+      type: 'string',
+      required: true
+    },
+    lastName: {
+      type: 'string',
+      required: true
+    },
+    email: {
+      type: 'email',
+      required: true,
+      unique: true
+    },
+    password: {
+      type: 'string',
+      required: true
+    }
+    // toJSON:function(){
+    //   var userObject = this.toObject();
+    //   delete userObject.password;
+    //   return userObject
+    // }
+  },
+  beforeCreate:function(value, cb){
+    bcrypt.hash(value.password, 10, function(err, hash){
+      if(err) return cb(err);
+      value.password = hash;
+      cb();
+    })
   }
 };
 
