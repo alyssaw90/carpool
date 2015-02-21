70db13bdac1a97cf7e02692ed4291889b93d87f5
Alyssa <alyssaw90@gmail.com>
2015-02-21 14:55:54 -0800

    map on show page

--- carpool/assets/js/controllers/RideShowCtrl.js
+++ carpool/assets/js/controllers/RideShowCtrl.js
@@ -1,4 +1,4 @@
-carpoolApp.controller('RideShowCtrl', ['$scope', '$http', '$routeParams', '$location', function($scope,$http,$routeParams,$location){
+carpoolApp.controller('RideShowCtrl', ['$scope', '$http', '$routeParams', '$location', 'uiGmapGoogleMapApi', function($scope,$http,$routeParams,$location,uiGmapGoogleMapApi){
 
   var rideId = $routeParams.id;
   $http.get('/api/ride/'+rideId).success(function(data){
@@ -6,6 +6,23 @@ carpoolApp.controller('RideShowCtrl', ['$scope', '$http', '$routeParams', '$loca
   }).error(function(err){
     $location.path('/');
     alert('Ride could not be found.');
+  });
+
+  angular.extend($scope, {
+    map: {
+      center: {
+        latitude: 43.7543352,
+        longitude:  -98.8630678
+      },
+      options: {
+        maxZoom: 20,
+        minZoom: 3
+      },
+      zoom: 4,
+      control: {},
+      markers: []
+    },
+    address: ''
   });
 
 }])
