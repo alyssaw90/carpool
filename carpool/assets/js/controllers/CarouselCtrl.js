7cb9aa4c438b0311f7bad8a448afb26254c614b5
Alyssa <alyssaw90@gmail.com>
2015-02-22 12:31:09 -0800

    home page fonts

--- carpool/assets/js/controllers/CarouselCtrl.js
+++ carpool/assets/js/controllers/CarouselCtrl.js
@@ -2,8 +2,8 @@ carpoolApp.controller('CarouselCtrl', function ($scope) {
   $scope.myInterval = 3000;
   $scope.slides = [
     {image:'/images/carpool-home.jpg',text:'Looking to set up a carpool?'},
-    {image:'/images/carpool-lights.jpg',text:'Look no further'},
-    {image:'/images/carpool-night.jpg',text:'This site will help you start or join a carpool'}
+    {image:'/images/carpool-lights.jpg',text:'Look no further!'},
+    {image:'/images/carpool-night.jpg',text:'Start or join a carpool here.'}
   ];
 
 });
