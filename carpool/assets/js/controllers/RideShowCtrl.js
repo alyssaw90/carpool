b172f2a2b68d4ae16c19f8e887165dd4de9e840b
Alyssa <alyssaw90@gmail.com>
2015-02-22 15:46:35 -0800

    join button stopping at maxRider amount

--- carpool/assets/js/controllers/RideShowCtrl.js
+++ carpool/assets/js/controllers/RideShowCtrl.js
@@ -4,23 +4,24 @@ carpoolApp.controller('RideShowCtrl', ['$scope', '$http', '$routeParams', '$loca
   $http.get('/api/ride/'+rideId).success(function(data){
     console.log(data)
     $scope.ride = data;
+    $scope.maxRiders = data.maxRiders
+    console.log($scope.maxRiders)
   }).error(function(err){
     $location.path('/');
     alert('Ride could not be found.');
   });
 
   $scope.startingValue = $scope.startingValue || 0
-  $scope.maxRiders = $scope.maxRiders
-
-  // $scope.joinButton = function(){
-  //   if($scope.startingValue !== $scope.maxRiders){
-  //     $scope.startingValue + 1
-  //   }
-  // }
 
   $scope.joinButton = function(){
-    $scope.startingValue = $scope.startingValue + 1
+    if($scope.startingValue != $scope.maxRiders){
+      $scope.startingValue = $scope.startingValue + 1
+    }
   }
+
+  // $scope.joinButton = function(){
+  //   $scope.startingValue = $scope.startingValue + 1
+  // }
 
   angular.extend($scope, {
     map: {
