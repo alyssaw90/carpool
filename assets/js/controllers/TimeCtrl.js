2476feccc0893b3d347905166c5f0686ee0cfd66
Alyssa <alyssaw90@gmail.com>
2015-03-09 14:38:13 -0700

    timepicker

---
+++ assets/js/controllers/TimeCtrl.js
@@ -0,0 +1,18 @@
+carpoolApp.controller('TimeCtrl',['$scope', '$log', function ($scope, $log) {
+  $scope.mytime = new Date();
+
+  $scope.hstep = 1;
+  $scope.mstep = 15;
+
+  $scope.options = {
+    hstep: [1, 2, 3],
+    mstep: [1, 5, 10, 15, 25, 30]
+  };
+
+  $scope.ismeridian = true;
+  $scope.toggleMode = function() {
+    $scope.ismeridian = ! $scope.ismeridian;
+  };
+
+
+}]);
